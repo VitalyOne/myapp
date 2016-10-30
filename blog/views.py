5767d4f99a06eada7776f46df35faca3689b41a3
VitalyOne <supamega@mail.ru>
2016-10-30 23:09:26 +0300

    post numbers

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -9,3 +9,6 @@ def post_list(request):
 def post_list_about(request):
 	a = 'hello! Jango 1.10'
 	return render(request, 'blog/post_list_about.html', {'a': a})
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
